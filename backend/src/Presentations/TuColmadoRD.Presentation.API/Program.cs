1d4afde05f9f64c7703116ae0c4592f0a59686e2
fcastro_dev <borrome941@gmail.com>
2026-04-24 18:40:34 -0400

    synchronization

--- backend/src/Presentations/TuColmadoRD.Presentation.API/Program.cs
+++ backend/src/Presentations/TuColmadoRD.Presentation.API/Program.cs
@@ -17,7 +17,6 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        // Entry point for standalone running
         if (args.Any(a => a.Contains("TuColmadoRD.Presentation.API.dll")) || args.Length == 0)
         {
             CoreApiHostBuilder.BuildCoreApi(args).Run();
